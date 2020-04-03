05b36c76bee6f3df2cd9fd2b5f153bbd89f3b54e
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-04-03 11:35:30 +0200

    Liten ändring i Garage

--- Garage1/Garage.cs
+++ Garage1/Garage.cs
@@ -6,6 +6,14 @@ namespace Garage1
     internal class Garage<T> : IEnumerable<T> where T : Vehicle
     {
         private Vehicle[] vehicles;
+        int capacity;
+
+        public Garage(int aCapacity)
+        {
+            capacity = aCapacity;
+            vehicles = new Vehicle[capacity];
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
             throw new System.NotImplementedException();
@@ -13,7 +21,7 @@ namespace Garage1
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return GetEnumerator();
         }
     }
 }
