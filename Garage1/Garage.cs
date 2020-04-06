a5f21820729a2cc19ff5f9fdb9287ba750460d71
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-04-06 16:49:01 +0200

    AddTo() and RemoveFrom() implemented in Garage.

--- Garage1/Garage.cs
+++ Garage1/Garage.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Garage1
 {
     internal class Garage<T> : IEnumerable<T> where T : Vehicle
     {
-        private Vehicle[] vehicles;
+        private T[] vehicles;
 
         private int capacity;
-              
+
+        public int VehiclesInGarage { get; set; }
+
         public Garage(int capacity)
         {
             this.capacity = capacity;
-            vehicles = new Vehicle[this.capacity];
+            vehicles = new T[this.capacity];
+            vechiclesInGarage = 0;
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -28,16 +32,54 @@ namespace Garage1
            return this.GetEnumerator();
         }
 
-        // Adds a Vehicle to the garage
-        public void AddTo(Vehicle vehicle)
+        // Adds a Vehicle to the garage.
+        // If the garage is full do nothing, otherwise
+        // add the vehicle to the garage.
+ 
+        public void AddTo(T vehicle)
         {
-            
+            if (!GarageFull())
+            {
+                for (int vehicleIndex = 0; vehicleIndex < VehiclesInGarage; vehicleIndex++)
+                {
+                    if (vehicles[vehicleIndex] == null)
+                    {
+                        vehicles[vehicleIndex] = vehicle;
+                        VehiclesInGarage++;
+                        return;
+                    }
+                }
+            }
         }
 
-        // Removes a Vehicle from the garage
-        public void RemoveFrom(Vehicle vehicle)
+        private bool GarageFull()
         {
+            return VehiclesInGarage == capacity;
+        }
 
+        // Removes a Vehicle from the garage.
+        // If the garage is empty or the vehicle in not found in
+        // the garage do nothing, otherwise remove the vehicle from the
+        // garage.
+        public void RemoveFrom(T vehicle)
+        {
+            if (!GarageEmpty())
+            {
+                for (int vehicleIndex = 0; vehicleIndex < VehiclesInGarage; vehicleIndex++)
+                {
+                    if (vehicles[vehicleIndex] == vehicle)
+                    {
+                        vehicles[vehicleIndex] = null;
+                        VehiclesInGarage--;
+                        return;
+                    }
+                }
+             }
+        }
+
+        private bool GarageEmpty()
+        {
+            return VehiclesInGarage == 0;
         }
     }
 }
